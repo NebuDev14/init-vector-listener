5df437bc786ca2d068600d0c8a17d8a56f707f4b
warren yun <wyun2006@gmail.com>
2023-07-03 00:25:45 -0400

    base listener

--- listener/listener.go
+++ listener/listener.go
@@ -2,9 +2,42 @@ package listener
 
 import (
 	"fmt"
-	"flag"
 	"io"
 	"log"
 	"net"
 	"os"
 )
+
+func start() {
+	host := "localhost"
+	port := 1155
+	addr := fmt.Sprintf("%s:%d", host, port)
+
+	listener, err := net.Listen("tcp", addr)
+
+	if err != nil {
+		panic(err)
+	}
+
+	log.Printf("Server started on %s", listener.Addr().String())
+
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			log.Printf("Error accepting connection: %s", err)
+		} else {
+			go acceptClient(conn)
+		}
+	}
+
+}
+
+func acceptClient(conn net.Conn) {
+	_, err := io.Copy(os.Stdout, conn)
+	
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	conn.Close()
+}
